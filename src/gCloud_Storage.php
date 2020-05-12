085e2350217e9830c8347b6a5545c7d1bf234fa9
Antonio Turdo <aturdo@schema31.it>
2020-05-12 18:02:45 +0200

    Fix versione

--- src/gCloud_Storage.php
+++ src/gCloud_Storage.php
@@ -5,7 +5,7 @@ namespace Schema31\GCloudStorageSDK;
 /**
  * Classe per interagire con l'API REST di gCloud Storage
  *
- * @version 1.1.40
+ * @version 1.1.44
  * @package GCloud-SDK-PHP
  */
 /**
@@ -27,7 +27,7 @@ class gCloud_Storage {
      * @var string
      * @access public
      */
-    const VERSION = "gCloud_Storage 1.1.40 [Composer]";
+    const VERSION = "gCloud_Storage 1.1.44 [Composer]";
 
     /**
      * Mime-type standard da utilizzare quando non specificato
